f6c3bf3cdfbfb28b3acc3ccb58fb904cde57c42a
beast-javi3 <javiala0104@gmail.com>
2025-02-02 19:50:48 +0000

    5 commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+public class Input{
 
+    public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+
+      String name;
+      System.out.print("Enter your name: ");
+      name = input.nextLine();
+
+      int age;
+      System.out.print("Enter your age:");
+      age = input.nextInt();
+
+      float weight;
+      System.out.print("Enter your weight: ");
+      weight = input.nextFloat();
+
+      Boolean smoker;
+      System.out.print("Are you a smoker? True or False: ");
+      smoker = input.nextBoolean();
+
+      System.out.println("Name: " +name );
+      System.out.println("Age: " + age);
+      System.out.println("Weight: " + weight);
+      System.out.println("Are you a smoker: "+ smoker);
+
+    }
+
+}
